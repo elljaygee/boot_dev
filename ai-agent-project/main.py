a5cc9c690ddfc755c2f9797998b85b45afed6603
elljaygee <lilithknits@gmail.com>
2026-05-15 12:13:42 +0100

    final edits to ai-aget-project files

--- ai-agent-project/main.py
+++ ai-agent-project/main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.prompts import system_prompt
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -21,38 +21,72 @@ args = parser.parse_args()
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 def main():
-    for attempt in range(5):
-        try:
-            response = client.models.generate_content(
-                model='gemini-2.5-flash',
-                contents=messages,
-                config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
-            )
+    for _ in range(20):
+        final_response = False
 
-            if not response.usage_metadata:
-                raise RuntimeError("No metadata found")
-            
-            else:
-                prompt_tokens = response.usage_metadata.prompt_token_count
-                response_tokens = response.usage_metadata.candidates_token_count
-                user_prompt = args.user_prompt
+        for attempt in range(5):
+            try:
+                response = client.models.generate_content(
+                    model='gemini-2.5-flash',
+                    contents=messages,
+                    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+                )
 
-                if args.verbose == True:
-                    print(f"User prompt: {user_prompt}")
-                    print(f"Prompt tokens: {prompt_tokens}")
-                    print(f"Response tokens: {response_tokens}")
+                if response.candidates:
+                    for candidate in response.candidates:
+                        messages.append(candidate.content)
 
-                if response.function_calls:
-                    for function_call in response.function_calls:
-                        print(f"Calling function: {function_call.name}({function_call.args})")
+                if not response.usage_metadata:
+                    raise RuntimeError("No metadata found")
+                
                 else:
-                    print(response.text)
+                    prompt_tokens = response.usage_metadata.prompt_token_count
+                    response_tokens = response.usage_metadata.candidates_token_count
+                    user_prompt = args.user_prompt
 
-                break
+                    if args.verbose == True:
+                        print(f"User prompt: {user_prompt}")
+                        print(f"Prompt tokens: {prompt_tokens}")
+                        print(f"Response tokens: {response_tokens}")
 
-        except Exception as e:
-            print(f"Attempt {attempt + 1} failed: {e}")
-            time.sleep(5)
+                    function_results = []
+
+                    if response.function_calls:
+
+                        for function_call in response.function_calls:
+                            function_call_result = call_function(function_call, verbose=args.verbose)
+                            if not function_call_result.parts:
+                                raise Exception(f"No parts in function call result: {function_call_result}")
+                            
+                            if not function_call_result.parts[0].function_response:
+                                raise Exception(f"No FunctionResponse object in function call result: {function_call_result}")
+                            
+                            if not function_call_result.parts[0].function_response.response:
+                                raise Exception(f"No function result in response: {function_call_result}")
+                            
+                            function_results.append(function_call_result.parts[0])
+            
+                            if args.verbose:
+                                print(f"-> {function_call_result.parts[0].function_response.response}")
+                            
+                            messages.append(types.Content(role="user", parts=function_results))
+
+                    else:
+                        print(response.text)
+                        final_response = True
+                        
+                        break
+                    
+            except Exception as e:
+                print(f"Attempt {attempt + 1} failed: {e}")
+                time.sleep(5)
+
+        if final_response:
+            break 
+
+    else:
+        print("Error: maximum iterations reached without a final response")
+        exit(1)
 
 if __name__ == "__main__":
     main()
